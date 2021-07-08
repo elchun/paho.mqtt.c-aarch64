6a645f37b6110344d0683d6e67ab83c11ab7b263
elchun <74155909+elchun@users.noreply.github.com>
2021-07-08 17:26:26 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 cp -r usr/local/bin/* /usr/local/bin
-cp -r usr/local/include/* usr/local/include
-cp -r usr/local/lib/* usr/local/lib
-cp -r usr/local/share/* usr/local/share
+cp -r usr/local/include/* /usr/local/include
+cp -r usr/local/lib/* /usr/local/lib
+cp -r usr/local/share/* /usr/local/share
 
 echo "paho.mqtt.c installed"
